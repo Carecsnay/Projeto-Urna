52a532bab1604c8b4755a9f8055f887950e9340e
Bruno Nitro 5 <carecsnay1@gmail.com>
2024-07-20 09:09:20 -0300

    Chore: Scripts added

---
+++ urna/stages.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "12345",
+        nome: "Ze Taxão",
+        partido: "PCC",
+        fotos: [{ url: "c02.jpeg", legenda: "Vereador" }],
+      },
+      {
+        numero: "54321",
+        nome: "Ze Milicia",
+        partido: "Miliciano",
+        fotos: [{ url: "c04.jpeg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "13",
+        nome: "Taxa do amor",
+        partido: "Taxação",
+        vice: "Ze Taxão",
+        fotos: [
+          { url: "c01.jpeg", legenda: "Prefeito" },
+          { url: "c02.jpeg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "17",
+        nome: "Arminha",
+        partido: "Joia",
+        vice: "Ze Milicia",
+        fotos: [
+          { url: "c03.jpeg", legenda: "Prefeito" },
+          { url: "c04.jpeg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
